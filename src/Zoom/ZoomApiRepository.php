449b26c3697973227e308815b72646c3a4cb63a3
Marijn Vandevoorde <marijn@teamleader.eu>
2020-06-11 00:57:57 +0200

    Log the right thing

--- src/Zoom/ZoomApiRepository.php
+++ src/Zoom/ZoomApiRepository.php
@@ -51,7 +51,7 @@ class ZoomApiRepository
         /** @var ResponseInterface $response */
         $createMeetingResponse = $this->oauthProvider->getResponse($request);
         $data = json_decode($createMeetingResponse->getBody()->getContents(), true);
-        $this->logger->debug($data);
+        $this->logger->debug("zoom response", $data);
         return new ZoomMeeting(
             $data['start_url'],
             $data['join_url']
